efd6b25c44e494843cf27796d228423ee31e0c1d
LuSantos36 <luciana.1lks@gmail.com>
2024-11-19 00:42:22 +0000

    aula1

---
+++ aula_2.py
@@ -0,0 +1,9 @@
+faturamento = 1200
+custo = 700
+lucro = faturamento - custo
+margem_lucro = lucro / faturamento
+
+#print("Faturamento da empresa: ", faturamento, "custo: ", custo, "lucro: ", lucro)
+print(f"Faturamento da empresa: R${faturamento:.2f}, Custo: R${custo:.2f}, Lucro:  R${lucro:.2f}")
+
+email_cliente = "email@gmail.com"
